828f09569cf05353878a9c80b24916bf1d20ab4d
hriti <hriti@HRITIKNAGPURE>
2024-12-16 18:08:33 +0530

    Add project files.

---
+++ LoginFormASPcore/Models/UserTbl.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginFormASPcore.Models
+{
+    public class UserTbl
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Gender { get; set; } = null!;
+        public int Age { get; set; }
+        public string Email { get; set; } = null!;
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
+}
